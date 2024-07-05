408bbb435f296ba62bc4737c5f7ae991b9316a77
chinta-ajay <chintaajay.22.it@anits.edu.in>
2024-07-05 15:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+    // Here you can add code to handle form submission, like sending an email or storing data.
+});
